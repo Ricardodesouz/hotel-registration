56701235b07c462aa1694576dbcd9129a23f2e11
Ricardodesouz <ricardodesouza360@gmail.com>
2024-03-23 20:25:37 -0300

    adicao do metdodo checkingDate

--- src/model/entities/Reservation.java
+++ src/model/entities/Reservation.java
@@ -23,5 +23,18 @@ public class Reservation {
     public void setGuest(Guest guest) {
         this.guest = guest;
     }
+    private void checkingDates(LocalDateTime checkIn, LocalDateTime checkout){
+        Duration lengthSay = Duration.between(checkIn, checkout);
+        if(lengthSay.toSeconds() <= 0){
+            throw new ReservationExeception("A data de saida não pode ser antes que a de entrada");
+        }
+        if(lengthSay.toHours() < 24){
+            throw new ReservationExeception("Desculpe, mas o tempo minimo de estadia é um dia");
+        }
+        if(lengthSay.toDays() > 200){
+            throw new ReservationExeception("O tempo de estadia não e suportado por nossas capicidades");
+        }
+
+    }
 
 }
