e6a9b5b6d9b526b6bf85d00e01db52b271003875
Ricardodesouz <ricardodesouza360@gmail.com>
2024-04-04 20:38:02 -0300

    commit para fazer o merge

--- src/application/Main.java
+++ src/application/Main.java
@@ -4,6 +4,8 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        /* nessa parte sera implementa uma interface grafica  usando javaFX para o usario 
+        apenas tenho que aprender a fazer isso primeiro :)*/
 
     }
 }
